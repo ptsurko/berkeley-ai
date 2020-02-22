063eea3efe427b3295571f290ea97e46c3477d8e
Pavel Tsurko <ptsurko@users.noreply.github.com>
2020-02-22 11:48:40 -0800

    Added reinforcement client

--- mdp/mdp.py
+++ mdp/mdp.py
@@ -1,110 +1,186 @@
+from collections import defaultdict
+
 def expected_utility(mdp, values, state, action):
-    return sum(t * values[new_state] for (t, new_state) in mdp.T(state, action))
+    return sum(prob * values[new_state] for (new_state, prob) in mdp.transitions(state, action))
 
 argmax = max
 
-def value_iteration(mdp, epsilon=0.001):
-    convergence = False
-    values = {state: 0 for state in mdp.states()}
 
-    gamma = mdp.gamma
+class Agent:
+    def __init__(self, gamma):
+        self.gamma = gamma
+        self.values = defaultdict(int)
 
-    while not convergence:
-        new_values = values.copy()
-        delta = 0
-        for state in mdp.states():
-            actions = mdp.actions(state)
-            max_q_value = -float('inf')
+    def get_q_value(self, mdp, state, action):
+        transitions = mdp.transitions(state, action)
+        if not len(transitions):
+            return 0
 
-            for action in actions:
-                q_value = sum(t * values[new_state] for (t, new_state) in mdp.T(state, action))
+        return sum(prob * (mdp.get_reward(state) + self.gamma * self.values[new_state]) for (new_state, prob) in mdp.transitions(state, action))
 
-                max_q_value = max(q_value, max_q_value)
+    def get_value(self, mdp, state):
+        actions = mdp.actions(state)
+        if not len(actions):
+            return 0
 
-            value = mdp.R(state) + gamma * (max_q_value or 0)
-            new_values[state] = value
-
-            delta = max(delta, abs(values[state] - new_values[state]))
-
-        values = new_values
-        if delta <= epsilon * (1 - gamma) / gamma:
-            return new_values
+        return max(self.get_q_value(mdp, state, action) for action in actions)
 
 
-def policy_evaluation(mdp, policy, max_k=20):
-    values = {state: 0 for state in mdp.states()}
+    def learn(self, mdp):
+        raise NotImplementedError("Please Implement this method")
 
-    for k in range(max_k):
-        new_values = values.copy()
+class ValueIterationAgent(Agent):
+    def __init__(self, epsilon=0.001, max_iterations=None, gamma=0.9):
+        super().__init__(gamma) 
+        self.epsilon = epsilon
+        self.max_iterations = max_iterations
 
-        for state in mdp.states():
-            q_value = expected_utility(mdp, values, state, policy[state])
+    def learn(self, mdp):
+        convergence = False
+        self.values = defaultdict(int)
 
-            value = mdp.R(state) + mdp.gamma * q_value
-            new_values[state] = value
+        iteration = 0
+        while not convergence:
+            new_values = defaultdict(int)
 
-        values = new_values
+            delta = 0
+            for state in mdp.states():
+                new_values[state] = self.get_value(mdp, state)
+                delta = max(delta, abs(new_values[state] - new_values[state]))
 
-    return values
+            self.values = new_values
+            iteration += 1
 
-
-def policy_iteration(mdp, initial_action):
-    policy = {state: initial_action for state in mdp.states()}
-    while True:
-        changed = False
-
-        values = policy_evaluation(mdp, policy)
-        new_policy = policy.copy()
-        for state in mdp.states():
-            max_action = argmax(
-                mdp.actions(state),
-                key=lambda action: sum(t * values[new_state] for (t, new_state) in mdp.T(state, action))
-            )
-
-            new_policy[state] = max_action
-            if policy[state] != new_policy[state]:
-                changed = True
-
-        policy = new_policy
-
-        if not changed:
-            return values
+            if self.max_iterations is not None and iteration >= self.max_iterations:
+                convergence = True
+            if self.max_iterations is None and delta <= self.epsilon * (1 - self.gamma) / self.gamma:
+                convergence = True
 
 
+class PolicyIterationAgent(Agent):
+    def __init__(self, initial_action, gamma=0.9):
+        super().__init__(gamma) 
+        self.initial_action = initial_action
+
+    def policy_evaluation(self, mdp, policy, max_k=9):
+        values = defaultdict(int)
+
+        for _ in range(max_k):
+            new_values = values.copy()
+
+            for state in mdp.states():
+                new_values[state] = self.get_value(mdp, state)
+
+            values = new_values
+
+        return values
+
+    def learn(self, mdp):
+        policy = {state: self.initial_action for state in mdp.states()}
+
+        while True:
+            changed = False
+
+            self.values = self.policy_evaluation(mdp, policy)
+            new_policy = policy.copy()
+            for state in mdp.states():
+                actions = mdp.actions(state)
+                max_action = None
+                if len(actions):
+                    max_action = argmax(
+                        mdp.actions(state),
+                        key=lambda action: self.get_q_value(mdp, state, action)
+                    )
+
+                new_policy[state] = max_action
+                if policy[state] != new_policy[state]:
+                    changed = True
+
+            policy = new_policy
+
+            if not changed:
+                break
+
+
+class ReinforcementAgent(Agent):
+    def __init__(self, initial_state=None, samples=[], alpha=0.9, gamma=1):
+        super().__init__(gamma) 
+
+        self.samples = samples
+        self.initial_state = initial_state
+        self.alpha = alpha
+
+
+    def get_q_value(self, mdp, state, action):
+        transitions = mdp.transitions(state, action)
+        if not len(transitions):
+            return 0
+
+        return sum(prob * (mdp.get_reward(state) + self.gamma * self.values[new_state]) for (new_state, prob) in mdp.transitions(state, action))
+
+    def get_value(self, mdp, state):
+        actions = mdp.actions(state)
+        if not len(actions):
+            return 0
+
+        return max(self.get_q_value(mdp, state, action) for action in actions)
+
+    def learn(self, mdp):
+        self.values = {state: 0 for state in mdp.states()}
+        self.q_values = {state: [0] * len(mdp.actions(state)) for state in mdp.states()}
+        
+        for sample in self.samples:
+            state = self.initial_state
+            new_values = {}
+            new_q_values = {key: value.copy() for key, value in self.q_values.items()}
+
+            for action in sample:
+                next_state = mdp.next_state(state, action)
+                actions = mdp.actions(state)
+                action_index = actions.index(action)
+
+                sample = mdp.get_reward(state) + self.gamma * (max(self.q_values[next_state]) if len(self.q_values[next_state]) else 0)
+                q_value = (1 - self.alpha) * self.q_values[state][action_index] + self.alpha * sample
+                
+                new_q_values[state][action_index] = q_value
+                new_values[state] = max(new_q_values[state])
+                state = next_state
+
+            self.values = new_values
+            self.q_values = new_q_values
+    
+
+# Rewrite using q-values
 def best_policy(mdp, values):
     return [
         (state, argmax(
             mdp.actions(state),
-            key=lambda action: sum(t * values[new_state] for (t, new_state) in mdp.T(state, action))))
-        for state in mdp.states()
+            key=lambda action: sum(t * values[new_state] for (new_state, t) in mdp.transitions(state, action))))
+        for state in mdp.states() if state is not Mdp.TERMINAL
     ]
 
 
 class Mdp:
-    def __init__(self, transitions, rewards, terminals=[], gamma=1):
-        self._transitions = transitions
-        self._rewards = rewards
-        self._terminals = terminals
-        self.gamma = gamma
+    EXIT = 'Exit'
+    TERMINAL = (None,)
 
     def states(self):
-        return self._transitions.keys()
+        raise NotImplementedError("Please Implement this method")
 
-    def T(self, state, action):
-        if state in self._terminals or action is None:
-            return [(0, state)]
+    def transitions(self, state, action):
+        raise NotImplementedError("Please Implement this method")
 
-        return [(t, probable_state) for probable_state, t in self._transitions[state][action]]
+    def get_reward(self, state):
+        raise NotImplementedError("Please Implement this method")
 
-    def R(self, state):
-        return self._rewards[state]
+    def isTerminal(self, state):
+        return state is Mdp.TERMINAL
 
     def actions(self, state):
-        if state in self._terminals:
-            return [None]
+        raise NotImplementedError("Please Implement this method")
 
-        return self._transitions[state].keys()
-
+    def next_state(self, state, action):
+        raise NotImplementedError("Please Implement this method")
 
 class CarRacingMdp(Mdp):
     pass
@@ -115,84 +191,104 @@ class GridMove:
     RIGHT = 'Right'
     BOTTOM = 'Bottom'
     LEFT = 'Left'
+    EXIT = Mdp.EXIT
 
 
 class GridMdp(Mdp):
-    def __init__(self, grid, terminals=[], gamma=1, probability=0.8):
-        self._grid = grid
-        transitions = {}
-        rewards = {}
-        actions = [GridMove.TOP, GridMove.RIGHT, GridMove.BOTTOM, GridMove.LEFT]
+    def __init__(self, grid, living_reward=0, noise=0.2):
+        self.grid = grid
+        self.living_reward = living_reward
+        self.noise = noise
 
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                state = (row, col)
-                if grid[row][col] is None:
-                    continue
+    def states(self):
+        states = [Mdp.TERMINAL]
+        for row in range(len(self.grid)):
+            for col in range(len(self.grid[0])):
+                if self.grid[row][col] is not None:
+                    states.append((row, col))
+        
+        return states
 
-                transition = {}
-                if (row, col) not in terminals:
-                    for action in actions:
-                        outcomes = []
+    def transitions(self, state, action):
+        outcomes = []
+        if state is not Mdp.TERMINAL:
+            for possible_action, action_probability in self.get_possible_actions(action, self.noise):
+                new_state = self.next_state(state, possible_action)
+                outcomes.append((new_state, action_probability))
 
-                        for possible_action, action_probability in GridMdp.get_possible_actions(action, probability):
-                            new_state = GridMdp.move_to(grid, (row, col), possible_action)
-                            outcomes.append((new_state, action_probability))
+        return outcomes
 
-                        transition[action] = outcomes
+    def get_reward(self, state):
+        if state == Mdp.TERMINAL:
+            return 0.0
 
-                transitions[state] = transition
-                rewards[state] = grid[row][col]
-
-        Mdp.__init__(self, transitions, rewards, terminals=terminals, gamma=gamma)
-
-    @staticmethod
-    def get_possible_actions(move, probability):
-        if move == GridMove.TOP:
-            return [(GridMove.TOP, probability), (GridMove.RIGHT, (1 - probability) / 2), (GridMove.LEFT, (1 - probability) / 2)]
-        if move == GridMove.RIGHT:
-            return [(GridMove.RIGHT, probability), (GridMove.TOP, (1 - probability) / 2), (GridMove.BOTTOM, (1 - probability) / 2)]
-        if move == GridMove.BOTTOM:
-            return [(GridMove.BOTTOM, probability), (GridMove.RIGHT, (1 - probability) / 2), (GridMove.LEFT, (1 - probability) / 2)]
-
-        return [(GridMove.LEFT, probability), (GridMove.TOP, (1 - probability) / 2), (GridMove.BOTTOM, (1 - probability) / 2)]
-
-
-    @staticmethod
-    def move_to(grid, state, move):
         row, col = state
-        max_row = len(grid) - 1
-        max_col = len(grid[0]) - 1
+        cell = self.grid[row][col]
+        if (type(cell) == int or type(cell) == float) and cell != 0:
+            return cell
+        return self.living_reward
+
+    def actions(self, state):
+        if state == Mdp.TERMINAL:
+            return []
+        row, col = state
+        cell = self.grid[row][col]
+        if (type(cell) == int or type(cell) == float) and cell != 0:
+            return [Mdp.EXIT]
+        return [GridMove.TOP, GridMove.RIGHT, GridMove.BOTTOM, GridMove.LEFT]
+
+    def get_possible_actions(self, move, noise):
         if move == GridMove.TOP:
-            if row - 1 >= 0 and grid[row - 1][col] is not None:
+            return [(GridMove.TOP, 1 - noise), (GridMove.RIGHT, noise / 2), (GridMove.LEFT, noise / 2)]
+        if move == GridMove.RIGHT:
+            return [(GridMove.RIGHT, 1 - noise), (GridMove.TOP, noise / 2), (GridMove.BOTTOM, noise / 2)]
+        if move == GridMove.BOTTOM:
+            return [(GridMove.BOTTOM, 1 - noise), (GridMove.RIGHT, noise / 2), (GridMove.LEFT, noise / 2)]
+        if move == GridMove.LEFT:
+            return [(GridMove.LEFT, 1 - noise), (GridMove.TOP, noise / 2), (GridMove.BOTTOM, noise / 2)]
+        if move == GridMove.EXIT:
+            return [(GridMove.EXIT, 1)]
+
+        raise Exception("{} move is not supported".format(move))
+        
+
+    def next_state(self, state, action):
+        row, col = state
+        max_row = len(self.grid) - 1
+        max_col = len(self.grid[0]) - 1
+        if action == GridMove.TOP:
+            if row - 1 >= 0 and self.grid[row - 1][col] is not None:
                 return (row - 1, col)
 
-        if move == GridMove.RIGHT:
-            if col + 1 <= max_col and grid[row][col + 1] is not None:
+        if action == GridMove.RIGHT:
+            if col + 1 <= max_col and self.grid[row][col + 1] is not None:
                 return (row, col + 1)
 
-        if move == GridMove.BOTTOM:
-            if row + 1 <= max_row and grid[row + 1][col] is not None:
+        if action == GridMove.BOTTOM:
+            if row + 1 <= max_row and self.grid[row + 1][col] is not None:
                 return (row + 1, col)
 
-        if move == GridMove.LEFT:
-            if col - 1 >= 0 and grid[row][col - 1] is not None:
+        if action == GridMove.LEFT:
+            if col - 1 >= 0 and self.grid[row][col - 1] is not None:
                 return (row, col - 1)
+
+        if action == GridMove.EXIT:
+            return Mdp.TERMINAL
 
         return state
 
     def to_action_grid(self, actions):
         chars = {
-            'Top': '^',
-            'Right': '>',
-            'Bottom': 'v',
-            'Left': '<',
-            None: '.',
+            GridMove.TOP: '^',
+            GridMove.RIGHT: '>',
+            GridMove.BOTTOM: 'v',
+            GridMove.LEFT: '<',
+            GridMove.EXIT: 'X',
         }
         result = []
-        for row in range(len(self._grid)):
+        for row in range(len(self.grid)):
             row_moves = []
-            for col in range(len(self._grid[0])):
+            for col in range(len(self.grid[0])):
                 for (state, action) in actions:
                     if state == (row, col):
                         row_moves.append(chars[action])
--- mdp/mdp_test.py
+++ mdp/mdp_test.py
@@ -1,145 +1,169 @@
 import unittest
 
-from mdp import CarRacingMdp, GridMdp, GridMove, value_iteration, policy_iteration, best_policy
-
+from mdp import Mdp, GridMdp, GridMove, ValueIterationAgent, PolicyIterationAgent, ReinforcementAgent, best_policy
 
 class TestSearch(unittest.TestCase):
-    # def test_car_mdp(self):
-    #
-    #     t = {
-    #         "Cool": {
-    #             "Slow": {
-    #                 "Cool1": (1.0, 1),
-    #             },
-    #             "Fast": {
-    #                 "Cool1": (0.5, 2),
-    #                 "Warm1": (0.5, 2),
-    #             },
-    #         },
-    #         "Warm": {
-    #             "Slow": {
-    #                 "Cool1": (0.5, 1),
-    #                 "Warm1": (0.5, 1),
-    #             },
-    #             "Fast": {
-    #                 "Overheated": (1.0, -10),
-    #             },
-    #         },
-    #         "Cool1": {
-    #             "Slow": {
-    #                 "Cool2": (1.0, 1),
-    #             },
-    #             "Fast": {
-    #                 "Cool2": (0.5, 2),
-    #                 "Warm2": (0.5, 2),
-    #             },
-    #         },
-    #         "Warm1": {
-    #             "Slow": {
-    #                 "Cool2": (0.5, 1),
-    #                 "Warm2": (0.5, 1),
-    #             },
-    #             "Fast": {
-    #                 "Overheated": (1.0, -10),
-    #             },
-    #         },
-    #         "Cool2": {
-    #             "exit": {
-    #                 "End": (1, 0),
-    #             }
-    #         },
-    #         "Warm2": {
-    #             "exit": {
-    #                 "End": (1, 0),
-    #             }
-    #         },
-    #         "Overheated": {},
-    #         "End": {}
-    #     }
-    #     mdp = CarRacingMdp(t)
-    #     values = value_iteration(mdp)
-    #
-    # def test_car_mdp2(self):
-    #
-    #     t = {
-    #         "Cool": {
-    #             "Slow": {
-    #                 "Cool": (1.0, 1),
-    #             },
-    #             "Fast": {
-    #                 "Cool": (0.5, 2),
-    #                 "Warm": (0.5, 2),
-    #             },
-    #         },
-    #         "Warm": {
-    #             "Slow": {
-    #                 "Cool": (0.5, 1),
-    #                 "Warm": (0.5, 1),
-    #             },
-    #             "Fast": {
-    #                 "Overheated": (1.0, -10),
-    #             },
-    #         },
-    #         "Overheated": {},
-    #     }
-    #     mdp = CarRacingMdp(t, terminals=["Overheated"], gamma=0.8)
-    #     values = value_iteration(mdp)
-
-    def test_grid_mdp(self):
-        mdp = GridMdp([[-0.04, -0.04, -0.04, +1],
-                       [-0.04, None, -0.04, -1],
-                       [-0.04, -0.04, -0.04, -0.04]],
-                      terminals=[(0, 3), (1, 3)], gamma=0.9)
-        values = value_iteration(mdp)
+    def test_value_iteration_values_2(self):
+        mdp = GridMdp([[0,    0, 0, +1],
+                       [0, None, 0, -1],
+                       [0,    0, 0,  0]])
+        agent = ValueIterationAgent(max_iterations=2)
+        agent.learn(mdp)
         self.assertEqual(
-            mdp.to_action_grid(best_policy(mdp, values)),
-            [['>', '>', '>', '.'],
-             ['^', '.', '^', '.'],
+            to_values_grid(mdp.grid, agent.values),
+            [['0.00', '0.00', '0.72',  '1.00'],
+             ['0.00',   None, '0.00', '-1.00'],
+             ['0.00', '0.00', '0.00',  '0.00']]
+        )
+
+    def test_value_iteration_values_5(self):
+        mdp = GridMdp([[0,    0, 0, +1],
+                       [0, None, 0, -1],
+                       [0,    0, 0,  0]])
+        agent = ValueIterationAgent(max_iterations=5)
+        agent.learn(mdp)
+        self.assertEqual(
+            to_values_grid(mdp.grid, agent.values),
+            [['0.51', '0.72', '0.84', '1.00'],
+             ['0.27', None, '0.55', '-1.00'],
+             ['0.00', '0.22', '0.37', '0.13']]
+        )
+
+    def test_value_iteration_values_100(self):
+        mdp = GridMdp([[0,    0, 0, +1],
+                       [0, None, 0, -1],
+                       [0,    0, 0,  0]])
+        agent = ValueIterationAgent(max_iterations=100)
+        agent.learn(mdp)
+        self.assertEqual(
+            to_values_grid(mdp.grid, agent.values),
+            [['0.64', '0.74', '0.85', '1.00'],
+             ['0.57', None, '0.57', '-1.00'],
+             ['0.49', '0.43', '0.48', '0.28']]
+        )
+
+    def test_value_iteration_values_100_with_liveness_reward(self):
+        mdp = GridMdp([[0,    0, 0, +1],
+                       [0, None, 0, -1],
+                       [0,    0, 0,  0]],
+                       living_reward=-0.1)
+        agent = ValueIterationAgent(max_iterations=100)
+        agent.learn(mdp)
+        self.assertEqual(
+            to_values_grid(mdp.grid, agent.values),
+            [['0.31', '0.51', '0.72',  '1.00'],
+             ['0.15', None,   '0.36', '-1.00'],
+             ['0.01', '0.01', '0.15', '-0.09']]
+        )
+
+    def test_value_iteration_q_values_100(self):
+        mdp = GridMdp([[0,    0, 0, +1],
+                       [0, None, 0, -1],
+                       [0,    0, 0,  0]])
+        agent = ValueIterationAgent(max_iterations=100)
+        agent.learn(mdp)
+        self.assertEqual(
+            to_q_values_grid(agent, mdp, agent.values),
+            [[['0.59', '0.64', '0.53', '0.57'], ['0.67', '0.74', '0.67', '0.60'], ['0.77',  '0.85', '0.57', '0.66'],                          ['1.00']],
+             [['0.57', '0.51', '0.46', '0.51'],                             None, ['0.57', '-0.60', '0.30', '0.53'],                         ['-1.00']],
+             [['0.49', '0.41', '0.44', '0.45'], ['0.40', '0.42', '0.40', '0.43'], ['0.48',  '0.29', '0.41', '0.40'], ['-0.65', '0.13', '0.27', '0.28']]]
+        )
+
+    def test_value_iteration_negative_rewards(self):
+        mdp = GridMdp([[0,    0, 0, +1],
+                       [0, None, 0, -1],
+                       [0,    0, 0,  0]], 
+                       living_reward=-0.04)
+        agent = ValueIterationAgent(max_iterations=100)
+        agent.learn(mdp)
+        self.assertEqual(
+            mdp.to_action_grid(best_policy(mdp, agent.values)),
+            [['>', '>', '>', 'X'],
+             ['^', '.', '^', 'X'],
              ['^', '>', '^', '<']]
         )
 
     def test_value_iteration_grid_mdp(self):
-        mdp = GridMdp([[0, 0, 0, +1],
+        mdp = GridMdp([[0,    0, 0, +1],
                        [0, None, 0, -1],
-                       [0, 0, 0, 0]],
-                      terminals=[(0, 3), (1, 3)], gamma=0.9)
-        values = value_iteration(mdp)
+                       [0,    0, 0,  0]])
+        agent = ValueIterationAgent(max_iterations=100)
+        agent.learn(mdp)
         self.assertEqual(
-            mdp.to_action_grid(best_policy(mdp, values)),
-            [['>', '>', '>', '.'],
-             ['^', '.', '^', '.'],
+            mdp.to_action_grid(best_policy(mdp, agent.values)),
+            [['>', '>', '>', 'X'],
+             ['^', '.', '^', 'X'],
              ['^', '<', '^', '<']]
         )
 
     def test_policy_iteration_grid_mdp(self):
-        mdp = GridMdp([[0, 0, 0, +1],
+        mdp = GridMdp([[0,    0, 0, +1],
                        [0, None, 0, -1],
-                       [0, 0, 0, 0]],
-                      terminals=[(0, 3), (1, 3)],
-                      gamma=0.9)
-        values = policy_iteration(mdp, GridMove.TOP)
+                       [0,    0, 0,  0]])
+        agent = PolicyIterationAgent(GridMove.TOP)
+        agent.learn(mdp)
         self.assertEqual(
-            mdp.to_action_grid(best_policy(mdp, values)),
-            [['>', '>', '>', '.'],
-             ['^', '.', '^', '.'],
-             ['^', '<', '^', '<']]
+            mdp.to_action_grid(best_policy(mdp, agent.values)),
+            [['>', '>', '>', 'X'],
+             ['^', '.', '^', 'X'],
+             ['^', '>', '^', '<']]
         )
 
     def test_policy_iteration_grid2_mdp(self):
         mdp = GridMdp([[-10, 100, -10],
                        [-10,   0, -10],
                        [-10,   0, -10],
-                       [-10,   0, -10]],
-                      terminals=[(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 2), (3, 0), (3, 2)],
-                      gamma=0.9)
-        values = policy_iteration(mdp, GridMove.TOP)
+                       [-10,   0, -10]])
+        agent = PolicyIterationAgent(GridMove.TOP)
+        agent.learn(mdp)
         self.assertEqual(
-            mdp.to_action_grid(best_policy(mdp, values)),
-            [['.', '.', '.'],
-             ['.', '^', '.'],
-             ['.', '^', '.'],
-             ['.', '^', '.']]
+            mdp.to_action_grid(best_policy(mdp, agent.values)),
+            [['X', 'X', 'X'],
+             ['X', '^', 'X'],
+             ['X', '^', 'X'],
+             ['X', '^', 'X']]
         )
+
+    def test_reinforcement_grid_agent(self):
+        mdp = GridMdp([[0,    0, 0, +1],
+                       [0, None, 0, -1],
+                       [0,    0, 0,  0]])
+        agent = ReinforcementAgent(
+            initial_state=(2, 0),
+            samples=[
+                [GridMove.TOP, GridMove.TOP, GridMove.RIGHT, GridMove.RIGHT, GridMove.RIGHT, GridMove.EXIT],
+                [GridMove.TOP, GridMove.TOP, GridMove.RIGHT, GridMove.RIGHT, GridMove.RIGHT, GridMove.EXIT],
+            ], 
+            alpha=0.5,
+            gamma=0.9)
+        agent.learn(mdp)
+        self.assertEqual(
+            to_values_grid(mdp.grid, agent.values),
+            [['0.00', '0.00', '0.23', '0.75'],
+             ['0.00',   None,   None,   None],
+             ['0.00',   None,   None,   None]]
+        )
+
+def to_values_grid(grid, values):
+    row_num, col_num = len(grid), len(grid[0])
+    values_grid = [[None] * (col_num) for _ in range(row_num)]
+    for row in range(row_num):
+        for col in range(col_num):
+            if (row, col) in values:
+                values_grid[row][col] = "{:.02f}".format(values[(row, col)])
+
+    return values_grid
+
+def to_q_values_grid(agent, mdp, values):
+    row_num, col_num = len(mdp.grid), len(mdp.grid[0])
+    q_values_grid = [[None] * (col_num) for _ in range(row_num)]
+    for row in range(row_num):
+        for col in range(col_num):
+            state = (row, col)
+            if state in values:
+                q_values_grid[row][col] = ["{:.02f}".format(agent.get_q_value(mdp, state, action)) for action in mdp.actions(state)]
+
+    return q_values_grid
 
 
 if __name__ == '__main__':
